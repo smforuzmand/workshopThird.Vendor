4d12fb1f6c0a164989ca659536a86b7470b477eb
Mohsen Forouzmand <94297998+smforuzmand@users.noreply.github.com>
2022-07-12 16:37:17 +0200

    Vending Machine Primary Concept :-(

---
+++ src/main/java/model/VendingMachineImpl.java
@@ -0,0 +1,33 @@
+package model;
+
+public class VendingMachineImpl implements VendingMachine{
+    @Override
+    public void addCurrency(int amount) {
+
+    }
+
+    @Override
+    public int getBalanace() {
+        return 0;
+    }
+
+    @Override
+    public Product request(int id) {
+        return null;
+    }
+
+    @Override
+    public int endSession() {
+        return 0;
+    }
+
+    @Override
+    public String getDescription(int id) {
+        return null;
+    }
+
+    @Override
+    public String[] getProduct() {
+        return new String[0];
+    }
+}
